65ee2f070384546344f81ad1baac70fe508986be
Yucheng Bai <2023lyan@sjtu.edu.cn>
2025-04-08 12:10:06 +0800

    add: p01b was finished

--- CS229/PS1/src/p01b_logreg.py
+++ CS229/PS1/src/p01b_logreg.py
@@ -15,6 +15,12 @@ def main(train_path, eval_path, pred_path):
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
 
     # *** START CODE HERE ***
+    x_eval, _ = util.load_dataset(eval_path, add_intercept=True)
+    clf = LogisticRegression(eps=1e-5)
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_eval)
+    np.savetxt(pred_path, y_pred, fmt="%d")
+    util.plot(x_train, y_train, clf.theta, f"output/p01b_pred_{pred_path[-5]}")
     # *** END CODE HERE ***
 
 
@@ -27,6 +33,12 @@ class LogisticRegression(LinearModel):
         > clf.predict(x_eval)
     """
 
+    def g(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def h(self, theta, x):
+        return self.g(np.matmul(theta, x))
+
     def fit(self, x, y):
         """Run Newton's Method to minimize J(theta) for logistic regression.
 
@@ -35,6 +47,27 @@ class LogisticRegression(LinearModel):
             y: Training example labels. Shape (m,).
         """
         # *** START CODE HERE ***
+        m = x.shape[0]
+        n = x.shape[1]
+        theta = np.zeros(n)
+        def d_l(theta):
+            sum_d_l = np.zeros(n)
+            for i in range(m):
+                sum_d_l = sum_d_l - 1 / m * (y[i] - self.h(theta, x[i,:])) * x[i,:]
+            return sum_d_l
+        def d_d_l(theta):
+            sum_d_d_l = np.zeros((n, n))
+            for i in range(m):
+                x_i = x[i,:].reshape(-1, 1)
+                sum_d_d_l = sum_d_d_l + 1 / m * self.h(theta, x[i,:]) * (1 - self.h(theta, x[i,:])) * np.matmul(x_i, x_i.T)
+            return sum_d_d_l
+        while True:
+            theta_decrease = np.matmul(np.linalg.inv(d_d_l(theta)), d_l(theta))
+            epsilon = self.eps
+            theta = theta - theta_decrease
+            if np.linalg.norm(theta_decrease) < epsilon:
+                break
+        self.theta = theta
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -47,4 +80,5 @@ class LogisticRegression(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        return self.g(np.matmul(self.theta, x.T)) > 0.5
         # *** END CODE HERE ***
